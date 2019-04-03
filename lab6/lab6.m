21c88fda54d86d438e2e601df772e85891dd8605
SangliTeng <37236559+SangliTeng@users.noreply.github.com>
2019-04-02 18:39:09 -0700

    Add files via upload

---
+++ lab6/lab6.m
@@ -0,0 +1,20 @@
+close;clear;clc;
+%% 1.3
+%% 1.4
+syms Fx Rx theta mg F_aero p Cd Af vd
+mdvdt = Fx - Rx - mg*sin(theta) - 1/2*p*Cd*Af*vd^2;
+jacobian(mdvdt,[vd,Fx])
+
+Rx = 5;
+Af = 0.15*0.25*0.9;
+Cd = 12.5;
+p = 1.225;
+g = 9.81;
+m = 5;
+A = -Af*Cd*p*0.5;
+B = 1;
+%% 1.5
+K = lqr(A,B,100,1)
+%% 1.6 simulink model
+sim('LQR_Blank.slx')
+%% 1.7 
